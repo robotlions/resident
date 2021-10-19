0d1bacaef6e17710b231b77c7b2799027907be1c
robotlions <>
2021-10-18 22:40:30 -0400

    updates splash photos and links

--- frontend/static/src/Components/Splash.js
+++ frontend/static/src/Components/Splash.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 import '../App.css';
-import feast from '../images/feast.JPEG';
+import menu from '../images/menu01.png';
+import about from '../images/about2.png';
+import location from '../images/location3.png';
+import catering from '../images/catering4.png';
+import merch from '../images/merch5.png';
 
 class Splash extends Component {
   constructor (props){
@@ -13,8 +18,15 @@ class Splash extends Component {
 
   return(
     <div>
-        <img className="splashPhoto" src={feast} alt="splash"></img>
-        {/* <div className="homeText"><h4>Optional Introductory Text</h4></div> */}
+      <NavLink to="/menu/">
+        <img className="splashPhoto" src={menu} alt="splash"></img></NavLink>
+        <NavLink to='/about/'>
+        <img className="splashPhoto" src={about} alt="splash"></img></NavLink>
+        <NavLink to='/schedule/'>
+        <img className="splashPhoto" src={location} alt="splash"></img></NavLink>
+        <NavLink to='/catering/'>
+        <img className="splashPhoto" src={catering} alt="splash"></img></NavLink>
+        <img className="splashPhoto" src={merch} alt="splash"></img>
     </div>
   );
 }
